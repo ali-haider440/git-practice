4fe71cb61956078e6e5a81965a9019d44c8ebf9b
ali-haider440 <colligiatecomputer@gmail.com>
2025-06-16 17:12:27 +0500

    firsty commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Button from '@mui/material/Button';
+import Navbar from './components/navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+ {/* <Button variant="contained">Hello world</Button>  */}
+     </>
+  )
+}
+
+export default App
